880e437084850cd7aed3d031fd0840443c8b6a2d
Sergiy Ignatenko <grydinywka@gmail.com>
2017-10-24 18:39:04 +0300

    detect heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
-import logging
-from logging.handlers import RotatingFileHandler
+import os
 
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
@@ -11,9 +10,14 @@ csrf = CSRFProtect(app)
 
 from app import views
 
+# define if on heroku environment
+ON_HEROKU = 'ON_HEROKU' in os.environ
+if not ON_HEROKU:
+    import logging
+    from logging.handlers import RotatingFileHandler
 
-formatter = logging.Formatter( "%(levelname)s %(asctime)s %(module)s: %(message)s")
-handler = RotatingFileHandler('app/logs/foo.log', maxBytes=100000, backupCount=5)
-handler.setLevel(logging.INFO)
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
+    formatter = logging.Formatter( "%(levelname)s %(asctime)s %(module)s: %(message)s")
+    handler = RotatingFileHandler('app/logs/foo.log', maxBytes=100000, backupCount=5)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
